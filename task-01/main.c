1773b4d6471cff67e209f7a5d8f9c849c08fa84c
Thanos Manolis <thanos.m14@gmail.com>
2020-10-19 12:12:10 +0300

    Add task-01 solution

--- task-01/main.c
+++ task-01/main.c
@@ -7,6 +7,8 @@ int main(void)
 {
     puts("This is Task-01");
 
+    printf("This application runs on %s\n", RIOT_BOARD);
+    
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 
